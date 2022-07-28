2cf6ee6dfd4a3d6fd3ddc0e9326aea7dbb9fef6e
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2022-07-28 22:16:15 +0530

    Demo Charts added

--- Web Development Frameworks/React/recharts-poc/src/App.js
+++ Web Development Frameworks/React/recharts-poc/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { RadarChart,PolarGrid, PolarAngleAxis, Radar, PolarRadiusAxis, Pie,PieChart, Scatter,ScatterChart, AreaChart, ComposedChart,Tooltip,Legend,Area, Bar,BarChart, ResponsiveContainer,LineChart, Line, XAxis, YAxis, CartesianGrid} from 'recharts';
+import Welcome from './components/Welcome';
 const data = [
   {
     name: 'Page A',
@@ -45,26 +46,143 @@ const data = [
   },
 ];
 
+const datas = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+];
+
+const dataa = [
+  {
+    subject: 'Math',
+    A: 120,
+    B: 110,
+    fullMark: 150,
+  },
+  {
+    subject: 'Chinese',
+    A: 98,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'English',
+    A: 86,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'Geography',
+    A: 99,
+    B: 100,
+    fullMark: 150,
+  },
+  {
+    subject: 'Physics',
+    A: 85,
+    B: 90,
+    fullMark: 150,
+  },
+  {
+    subject: 'History',
+    A: 65,
+    B: 85,
+    fullMark: 150,
+  },
+];
+
+const dataas = [
+  { x: 100, y: 200, z: 200 },
+  { x: 120, y: 100, z: 260 },
+  { x: 170, y: 300, z: 400 },
+  { x: 140, y: 250, z: 280 },
+  { x: 150, y: 400, z: 500 },
+  { x: 110, y: 280, z: 200 },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-          <AreaChart
-            data={data}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-          </AreaChart>
+      <div className='App' align='center'>
+
+        <h1>Demo Charts</h1>
+        <Welcome><h1>Hello</h1></Welcome>
+     
+      <BarChart width={500} height={500} data={data}>
+          <Bar dataKey="uv" fill="#8884d8" />
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+      </BarChart>
+  
+      <ComposedChart width={500} height={500} data={data}
+        margin={{
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 20,
+        }}
+        >
+      <CartesianGrid stroke="#f5f5f5" />
+      <XAxis dataKey="name" label={{ value: 'Pages', position: 'insideBottomRight', offset: 0 }} scale="band" />
+      <YAxis label={{ value: 'Index', angle: -90, position: 'insideLeft' }} />
+      <Tooltip />
+      <Legend />
+      <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+      <Bar dataKey="pv" barSize={20} fill="#413ea0" />
+      <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+      </ComposedChart>
+      
+          <AreaChart width={500} height={500} data={data} align='left'
+        margin={{
+        top: 20,
+        right: 20,
+        left: 20,
+        bottom: 20,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+    </AreaChart>
+
+      <PieChart width={500} height={500}>
+            <Pie dataKey="value" data={datas} fill="#8884d8" label />
+          </PieChart>
+
+          <RadarChart width={500} height={500} cx="50%" cy="50%" outerRadius="80%" data={dataa}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis />
+          <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+        </RadarChart>
+
+        <ScatterChart
+          width={500}
+          height={500}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter name="A school" data={dataas} fill="#8884d8" />
+        </ScatterChart>
+
+        
+
       </div>
     );
   }
