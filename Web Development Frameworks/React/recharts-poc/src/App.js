332722f8cb0a566a9e154bf109a1884ad56628c3
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2022-08-11 00:21:29 +0530

    Functions Updated

--- Web Development Frameworks/React/recharts-poc/src/App.js
+++ Web Development Frameworks/React/recharts-poc/src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import data from './components/cars.json';
-import LineChart from './components/LineChart';
-import AreaChart  from './components/AreaChart';
-import BarChart from './components/BarChart';
-import PieChart from './components/PieChart';
-import ScatterChart from './components/ScatterChart';
-import ComposedChart from './components/ComposedChart';
+import Line_Chart from './components/Line_Chart';
+import Area_Chart  from './components/Area_Chart';
+import Bar_Chart from './components/Bar_Chart';
+import Pie_Charts from './components/Pie_Charts';
+import Scatter_Chart from './components/Scatter_Chart';
+import Composed_Chart from './components/Composed_Chart';
 
 class App extends Component {
   state={
@@ -14,14 +14,14 @@ class App extends Component {
   render(){
     return (
 
-    <div className="App">
-      <LineChart name={this.state.mydata} x="Name" y="Displacement"/>
-      <AreaChart/>
-      <BarChart/>
-      <ComposedChart/>
-      <ScatterChart/>
-      <PieChart/>
-      
+    <div className="App" align="center">
+      <Line_Chart data={this.state.mydata} x="Name" y="Displacement"/>
+      <Area_Chart data={this.state.mydata} x="Origin" y="Horsepower"/>
+      <Bar_Chart data={this.state.mydata} x="Name" y="Weight_in_lbs"/>
+      <Composed_Chart data={this.state.mydata} x="Year" y1="Displacement" y2="Horsepower" y3="Accleratiom" y4="Weight_in_lbs"/>
+      <Scatter_Chart data={this.state.mydata} x="Displacement" y="Horsepower"/>
+      <Pie_Charts data={this.state.mydata} x="Cylinders"/>
+
     </div>
   );
 }}
