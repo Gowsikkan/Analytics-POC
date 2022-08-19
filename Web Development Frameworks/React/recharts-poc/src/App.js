316389caf695d76f1157572986e23d499095b8c4
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2022-08-19 23:16:47 +0530

    Updated

--- Web Development Frameworks/React/recharts-poc/src/App.js
+++ Web Development Frameworks/React/recharts-poc/src/App.js
@@ -1,56 +1,29 @@
 import React, {useState, useEffect} from 'react';
-function App() {
-  const [merchants, setMerchants] = useState(false);
-  useEffect(() => {
-    getMerchant();
-  }, []);
-  function getMerchant() {
-    fetch('http://localhost:3001')
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        setMerchants(data);
-      });
-  }
-  function createMerchant() {
-    let name = prompt('Enter merchant name');
-    let email = prompt('Enter merchant email');
-    fetch('http://localhost:3001/merchants', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({name, email}),
+import data from './components/cars.json';
+import Charts from './components/charts';
+
+const App=()=>{
+  
+const [merchants, setMerchants] = useState(false);
+
+useEffect(() => {
+getMerchant();
+}, []);
+function getMerchant() {
+  fetch('http://localhost:3001')
+    .then(response => {
+      return response.text();
     })
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        alert(data);
-        getMerchant();
-      });
-  }
-  function deleteMerchant() {
-    let id = prompt('Enter merchant id');
-    fetch(`http://localhost:3001/merchants/${id}`, {
-      method: 'DELETE',
-    })
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        alert(data);
-        getMerchant();
-      });
-  }
-  return (
-    <div>
-      <br />
-      <button onClick={createMerchant}>Add merchant</button>
-      <br />
-      <button onClick={deleteMerchant}>Delete merchant</button>
+    .then(data => {
+      setMerchants(data);
+    });
+}
+    return (
+    <div className="App" align="center">
+      {merchants}
+      <Charts data={data}/>
     </div>
   );
 }
+
 export default App;
