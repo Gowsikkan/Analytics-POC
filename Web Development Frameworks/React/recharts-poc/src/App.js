e6032b21a6d1d9d3d3d5c586c2b8a15cc665b29b
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2022-08-19 18:20:46 +0530

    Updated

--- Web Development Frameworks/React/recharts-poc/src/App.js
+++ Web Development Frameworks/React/recharts-poc/src/App.js
@@ -1,17 +1,56 @@
-import React, {Component} from 'react';
-import data from './components/cars.json';
-import Charts from './components/charts';
-
-class App extends Component {
-  state={
-    mydata:data,
+import React, {useState, useEffect} from 'react';
+function App() {
+  const [merchants, setMerchants] = useState(false);
+  useEffect(() => {
+    getMerchant();
+  }, []);
+  function getMerchant() {
+    fetch('http://localhost:3001')
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        setMerchants(data);
+      });
   }
-  render(){
-    return (
-    <div className="App" align="center">
-      <Charts data={this.state.mydata}/>
+  function createMerchant() {
+    let name = prompt('Enter merchant name');
+    let email = prompt('Enter merchant email');
+    fetch('http://localhost:3001/merchants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name, email}),
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getMerchant();
+      });
+  }
+  function deleteMerchant() {
+    let id = prompt('Enter merchant id');
+    fetch(`http://localhost:3001/merchants/${id}`, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getMerchant();
+      });
+  }
+  return (
+    <div>
+      <br />
+      <button onClick={createMerchant}>Add merchant</button>
+      <br />
+      <button onClick={deleteMerchant}>Delete merchant</button>
     </div>
   );
-}}
-
+}
 export default App;
